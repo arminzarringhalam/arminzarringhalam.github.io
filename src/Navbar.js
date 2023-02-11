d1d8915060b22b70478aa0814ef684f70a39efbe
Maziyar Dowlatabadibazaz <maziyardowlat@gmail.com>
2023-02-11 16:22:10 -0700

    Nav-bar links

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { Link} from "react-router-dom";
 import './Navbar.css';
 import Logo from "./assets/logo.png"
+import Spotify from "./assets/spotify.png"
+import Instagram from "./assets/insta.png"
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -29,9 +31,18 @@ function Navbar() {
             <div className='navbar-container'>
                 <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         <img src = {Logo} />
+                        {/* <img src ={Spotify} /> */}
                 </Link>
-                <div className="menu-icon" onClick={handleClick}>
-                    <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+                <div className="Logos" onClick={handleClick}>
+                    <a href="https://open.spotify.com/artist/5K4W6rqBFWDnAN6FQUkS6x">
+                        <img  src = {Spotify} />  
+                    </a>
+                    <a href="https://www.instagram.com/escape_to_safety/">
+                        <img className='insta' src = {Instagram} />
+                    </a>
+
+                        {/* <img  src = {Spotify} /> */}
+                    {/* <i className={click ? "fas fa-times" : "fas fa-bars"}/> */}
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 </ul>
